75cb563a090b635e22a409dc2bc4c0f5dd98cfac
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-02-14 07:17:17 -0800

    Apply suggestions from code review
    Apply feedback from CR
    
    Co-Authored-By: Steve Sanderson <SteveSandersonMS@users.noreply.github.com>

--- src/Components/Blazor/WebAssembly.Authentication/src/Services/IAccessTokenProvider.cs
+++ src/Components/Blazor/WebAssembly.Authentication/src/Services/IAccessTokenProvider.cs
@@ -13,14 +13,14 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
         /// <summary>
         /// Tries to get an access token for the current user with the default set of permissions.
         /// </summary>
-        /// <returns>A <see cref="ValueTask{AccessTokenResult}"/> that will return contain the <see cref="AccessTokenResult"/> when completed.</returns>
+        /// <returns>A <see cref="ValueTask{AccessTokenResult}"/> that will contain the <see cref="AccessTokenResult"/> when completed.</returns>
         ValueTask<AccessTokenResult> RequestAccessToken();
 
         /// <summary>
         /// Tries to get an access token with the options specified in <see cref="AccessTokenRequestOptions"/>.
         /// </summary>
         /// <param name="options">The <see cref="AccessTokenRequestOptions"/> for provisioning the access token.</param>
-        /// <returns>A <see cref="ValueTask{AccessTokenResult}"/> that will return contain the <see cref="AccessTokenResult"/> when completed.</returns>
+        /// <returns>A <see cref="ValueTask{AccessTokenResult}"/> that will contain the <see cref="AccessTokenResult"/> when completed.</returns>
         ValueTask<AccessTokenResult> RequestAccessToken(AccessTokenRequestOptions options);
     }
 }
